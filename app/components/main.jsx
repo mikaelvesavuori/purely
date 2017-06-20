cb3e3808bdde0467645cc14fab874f6e57281c20
Mikael Vesavuori <mikael.vesavuori@sogeti.com>
2017-06-20 08:27:23 +0200

    * Clean project * Add Google verification HTML

--- app/components/main.jsx
+++ app/components/main.jsx
@@ -9,12 +9,11 @@ import Products from "components/Products";
 // Import the SCSS if you"d rather use PostCSS/CSSnext/Sass
 //import "./../styles/main.scss";
 
-const API_BASE_URL = "https://cdn.contentful.com";
-const API_SPACE_ID = "650mcoaglyg0";
-const API_KEY = "ff77ee50877b1197c61a4baeea61b7da6d3d3a47667ba9da7c7b770e9d7a1258";
-
 import AppWrapper from "./AppWrapper";
 import Header from "components/Header";
+
+const API_SPACE_ID = "650mcoaglyg0";
+const API_KEY = "ff77ee50877b1197c61a4baeea61b7da6d3d3a47667ba9da7c7b770e9d7a1258";
 
 const client = contentful.createClient({
 	space: API_SPACE_ID,
@@ -26,31 +25,10 @@ class Main extends React.Component {
 		super(props);
 
 		this.state = {
-			headerTitle: "",
-			articles: [],
 			products: []
 		};
 
-		// Bindings
-		this.getAllArticles = this.getAllArticles.bind(this);
 		this.getAllProducts = this.getAllProducts.bind(this);
-	}
-
-	getAllArticles() {
-		client.getEntries({
-			"content_type": "artikel"
-		})
-		.then((entries) => {
-			const articles = [];
-
-			entries.items.forEach((entry) => {
-				articles.push(entry.fields.rubrik);
-			});
-
-			this.setState({
-				articles: articles
-			});
-		});
 	}
 
 	getAllProducts() {
@@ -73,12 +51,10 @@ class Main extends React.Component {
 	}
 
 	componentDidMount() {
-		this.getAllArticles();
 		this.getAllProducts();
 	}
 
 	render() {
-		const articles = this.state.articles;
 		const products = this.state.products;
 
 		return (
@@ -86,9 +62,6 @@ class Main extends React.Component {
 				<Header text="Header" />
 				<Routes />
 				<Products products={products} />
-				{articles.map(article => {
-					return <p><a href="">{article}</a></p>;
-				})}
 			</AppWrapper>
 		);
 	}
